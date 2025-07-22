68696d52ac4e0107424ba71a602d0215d7c68d58
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2025-07-22 16:37:26 +0800

    Feat (UI): Added UI for Quiz Page

---
+++ QuizLock/QuizLock.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace QuizLock
+{
+    partial class QuizLock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartQuiz = new System.Windows.Forms.Button();
+            this.btnEnterPasscode = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStartQuiz
+            // 
+            this.btnStartQuiz.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartQuiz.Location = new System.Drawing.Point(259, 223);
+            this.btnStartQuiz.Name = "btnStartQuiz";
+            this.btnStartQuiz.Size = new System.Drawing.Size(119, 40);
+            this.btnStartQuiz.TabIndex = 0;
+            this.btnStartQuiz.Text = "Start Quiz";
+            this.btnStartQuiz.UseVisualStyleBackColor = true;
+            // 
+            // btnEnterPasscode
+            // 
+            this.btnEnterPasscode.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnterPasscode.Location = new System.Drawing.Point(232, 283);
+            this.btnEnterPasscode.Name = "btnEnterPasscode";
+            this.btnEnterPasscode.Size = new System.Drawing.Size(170, 44);
+            this.btnEnterPasscode.TabIndex = 1;
+            this.btnEnterPasscode.Text = "Enter Passcode";
+            this.btnEnterPasscode.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(135, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(395, 74);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Start your quiz and have a \r\npassing score to use your computer";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(213, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(246, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "(Or you can just enter the passcode)";
+            // 
+            // QuizLock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 382);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEnterPasscode);
+            this.Controls.Add(this.btnStartQuiz);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "QuizLock";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quiz Lock";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.QuizLock_FormClosing);
+            this.Load += new System.EventHandler(this.QuizLock_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartQuiz;
+        private System.Windows.Forms.Button btnEnterPasscode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ QuizLock/QuizLock.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizLock
+{
+    public partial class QuizLock : Form
+    {
+        public QuizLock()
+        {
+            InitializeComponent();
+            this.MinimizeBox = false; // will prevent windows from minizing the size
+            this.MaximizeBox = false; // will prevent windows from maximizing the size
+        }
+
+        private void QuizLock_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true; // will prevent windows from closing
+        }
+
+        private void QuizLock_Load(object sender, EventArgs e)
+        {
+            this.TopMost = true; // will prevent windows from switching to another windows
+        }
+    }
+}
